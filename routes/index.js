c34db79d9333b55043b12dc062ebe9d72c188c6c
Chris Lee <sihrc.c.lee@gmail.com>
2013-11-06 16:09:27 -0500

    Added fake data to index.js

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,58 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index');
+var data = {
+  "entries": [
+    {
+      "title": "Chicken Noms",
+      "picture":"/images/shawarma.jpeg",
+      "notes": "fake notes",
+      "recipe": "fake recipe",
+      "category": "cat1#cat2#cat3",
+      "datecreated": "11/4/2013"
+    },
+    {
+      "title": "Chicken Noms2",
+      "picture":"/images/shawarma.jpeg2",
+      "notes": "fake notes2",
+      "recipe": "fake recipe2",
+      "category": "cat1#cat2#cat32",
+      "datecreated": "11/4/20132"
+    }
+  ]
 };
 
-exports.partials = function (req, res) {
-  var name = req.params.name;
-  res.render('partials/' + name);
+
+exports.entries = function (req, res) {
+var entries = [];
+  data.entries.forEach(function (entry, i) {
+    entries.push({
+      id: i,
+      title: entry.title,
+      picture: entry.picture,
+      notes: entry.notes,
+      recipe: entry.recipe,
+      category: entry.category,
+      datecreated: entry.datecreated,
+    });
+  });
+  res.json({
+    entries: entries
+  });
 };
+
+exports.entry = function (req, res) {
+  var id = req.params.id;
+  if (id >= 0 && id < data.entries.length) {
+    res.json({
+      entry: data.entries[id]
+    });
+  } else {
+    res.json(false);
+  }
+};
+
+exports.index = function(req, res){
+  res.render('index', entries);
+};
+
