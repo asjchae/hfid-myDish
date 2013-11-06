2292822ebf2c271cbd93b8fb14e5e087305503d3
Chris Lee <sihrc.c.lee@gmail.com>
2013-11-06 16:12:06 -0500

    deleted unnecessary code

--- routes/index.js
+++ routes/index.js
@@ -24,36 +24,6 @@ var data = {
   ]
 };
 
-
-exports.entries = function (req, res) {
-	var entries = [];
-	  data.entries.forEach(function (entry, i) {
-	    entries.push({
-	      id: i,
-	      title: entry.title,
-	      picture: entry.picture,
-	      notes: entry.notes,
-	      recipe: entry.recipe,
-	      category: entry.category,
-	      datecreated: entry.datecreated,
-	    });
-	  });
-	  res.json({
-	    entries: entries
-	  });
-};
-
-exports.entry = function (req, res) {
-  var id = req.params.id;
-  if (id >= 0 && id < data.entries.length) {
-    res.json({
-      entry: data.entries[id]
-    });
-  } else {
-    res.json(false);
-  }
-};
-
 exports.index = function(req, res){
   res.render('index', {entries: data.entries});
 };
