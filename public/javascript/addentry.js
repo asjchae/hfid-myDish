e6a5726deabfeb0ff074f76f4ae3c5bcb26dd48f
Chris Lee <sihrc.c.lee@gmail.com>
2013-11-07 16:05:26 -0500

    Addentry js

--- public/javascript/addentry.js
+++ public/javascript/addentry.js
@@ -1,11 +1,18 @@
 // Javascript for creating a new entry
 
 $(window).ready(function(){
+	$('#multiselect').multiselect();
+	$('#multiselect').
+	var value = $("#newGroup").val();
+	$('#multiselect').append("<option value=\"" + value + "\">" + value + "</option>");
+	$('#multiselect').multiselect('refresh');
+
 	$('#newentry').on('submit', function(){
 		var title = $('#title').val();
 		var picture = $('#picture').attr("src");
 		var recipe = $('#recipe').val();
 		var notes = $('#notes').val();
-		var category = $('#');
+		var category = $('#multiselect').getChecked();
 	})
 })
+
