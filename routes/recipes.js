006b585b298392effaa34862d9e158b386b39630
achae <chaeariana@gmail.com>
2013-11-09 18:01:59 -0500

    delete database functionality

--- routes/recipes.js
+++ routes/recipes.js
@@ -18,19 +18,9 @@ exports.db_entries = function (req, res) {
 
 
 exports.db_delete = function (req, res) {
-  var deleteAll = Entry.find({}).exec(function (err, data) {
-    if (err) {
-      res.send("Could not find all entries");
-    } else if (data.length == 0) {
-      res.send("No entries");
-    } else {
-      entry_compile(data, res, function (res, allentries) {
-        for (var i=0; i<allentries.length; i++) {
-          var entry = Entry.find({title: allentries[i].title}).remove();
-        }
-      });
-    }
-  });
+  var deleteAll = Entry.find({}).remove();
+  deleteAll.exec();
+  res.redirect('/db_entries');
 };
 
 function entry_compile(data, res, callback) {
