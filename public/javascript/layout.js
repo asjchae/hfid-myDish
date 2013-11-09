26ea6edf698ff4b42434ba74b8bf15d3954a33a8
Chris Lee <sihrc.c.lee@gmail.com>
2013-11-09 18:57:16 -0500

    Fixed Layout.js

--- public/javascript/layout.js
+++ public/javascript/layout.js
@@ -1,24 +1,21 @@
+$(window).ready(function(){
+	resizePage();
+	setSelected();
+});
+
 var Entry = require('../models/entry_schema')
     , mongoose = require('mongoose');
-
-//Resize the height of the page
 
 function resizePage(){
 	if ($('.body-container').height() < 600){
 		$('.body-container').height(600);
 	}
-	$('.title').text("hi");
 }
-
 
 function setSelected(){
 	$('#multiselect').onchange(function(){
 			var selected = $('#multiselect').options[$('#multiselect').selectedIndex].value;
-	$('.title').append(selected);
 	});
 }
 
-$(window).ready(function(){
-	resizePage();
-	setSelected();
-});
+
