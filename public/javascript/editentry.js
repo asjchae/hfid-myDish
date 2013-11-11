61f3186c4fa9da9af96b17131ed8ec20db167aa4
Chris Lee <sihrc.c.lee@gmail.com>
2013-11-10 21:43:58 -0500

    viewentry.jade is done no more changing

--- public/javascript/editentry.js
+++ public/javascript/editentry.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
-	populateEditEntry();
+        populateEditEntry();
 });
 
 function populateEditEntry(){
-	var cats = $('#catsdisplay').text();
-	var res = cats.split(",");
-	for (var i = 0; i < res.length; i++) {
-		var option = $("option[value='"+res[i]+"']");
-		console.log(option.val());
-		$("option[value='"+res[i]+"']").attr('selected','selected');
-	}
-	$('#multiselect').selectmenu("refresh",true);
+        var cats = $('#catsdisplay').text();
+        var res = cats.split(",");
+        for (var i = 0; i < res.length; i++) {
+                var option = $("option[value='"+res[i]+"']");
+                console.log(option.val());
+                $("option[value='"+res[i]+"']").attr('selected','selected');
+        }
+        $('#multiselect').selectmenu("refresh",true);
 }
 
 function editEntryCats(){
-	var categories = $("option:selected").map(function(){ return this.value }).get().join(", ");
-	$("#catsdisplay").text(categories);
+        var categories = $("option:selected").map(function(){ return this.value }).get().join(", ");
+        $("#catsdisplay").text(categories);
 }
